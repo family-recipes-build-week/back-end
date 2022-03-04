79e6e0bf80d57066203ca482243f6e4f784b66c0
Markeisha <markeisha94@gmail.com>
2022-03-04 14:25:54 -0600

    updated knexx

--- knexfile.js
+++ knexfile.js
@@ -1,27 +1,38 @@
-const sharedConfig = {
-  client: 'pg',
-  useNullAsDefault: true,
-  migrations: {
-    directory: './data/migrations',
-  },
-  seeds: {
-    directory: './data/seeds',
-  },
-  // this enables foreign keys in SQLite
-  pool: {
-    afterCreate: (conn, done) => {
-      conn.run('PRAGMA foreign_keys = ON', done)
-    },
-  },
-}
-
 module.exports = {
   development: {
-    ...sharedConfig,
-    connection: { filename: './data/recipes.sql' },
+    client: "sqlite3",
+    useNullAsDefault: true,
+    connection: {
+      filename: "./data/recipes.db3",
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
+      },
+    },
+    migrations: {
+      directory: "./data/migrations",
+    },
+    seeds: {
+      directory: "./data/seeds",
+    },
   },
-  testing: {
-    ...sharedConfig,
-    connection: { filename: './data/testing.db3' },
+  production: {
+    client: "sqlite3",
+    useNullAsDefault: true,
+    connection: {
+      filename: "./data/recipes.db3",
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
+      },
+    },
+    migrations: {
+      directory: "./data/migrations",
+    },
+    seeds: {
+      directory: "./data/seeds",
+    },
   },
-}
+};
