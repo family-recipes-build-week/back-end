a879ae43b8bd2ca38bd1c2360500240f43e4b803
Markeisha <markeisha94@gmail.com>
2022-03-04 14:05:54 -0600

    idk

--- data/db-config.js
+++ data/db-config.js
@@ -1,6 +1,6 @@
-const knex = require('knex');
+const knex = require('knex')({client: 'pg'});
 const configs = require('../knexfile');
 // eslint-disable-next-line no-undef
 const environment = process.env.NODE_ENV || 'development';
 
-module.exports = knex(configs[process.env.NODE_ENV]);
+module.exports = knex(configs[environment]);
