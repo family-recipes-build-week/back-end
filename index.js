4e9e56e7f645e40e3f93f379fac0e2553bfb2f74
Markeisha <markeisha94@gmail.com>
2022-03-04 14:30:45 -0600

    index

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
-const express = require('express');
-const server = require('./api/server');
+// const express = require('express');
+// const server = require('./api/server');
+
+// require('dotenv').config();
+
+// const PORT = process.env.PORT || 9000
+
+// server.listen(PORT, () =>  {
+//     console.log(`listening on port ${PORT}`)
+// });
+
+// server.use('*', (req, res) => {
+//     res.send('<h1>Hello Heroku</h1>')
+// })
 
 require('dotenv').config();
 
-const PORT = process.env.PORT || 9000
+const server = require('./api/server');
 
-server.listen(PORT, () =>  {
-    console.log(`listening on port ${PORT}`)
-});
+const port = process.env.PORT || 9000;
+const secret = process.env.SECRET_THING || 'foo';
 
-server.use('*', (req, res) => {
-    res.send('<h1>Hello Heroku</h1>')
+console.log(port, secret);
+
+server.listen(port, () => {
+    console.log(`\n*** Server running on ${port} ***\n`)
 })
