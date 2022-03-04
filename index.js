a038abe2d5a0a6f566bd19c7537d8df5828acd8f
Markeisha <markeisha94@gmail.com>
2022-03-04 13:49:57 -0600

    index

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-require('dotenv').config();
+const express = require('express');
+const server = require('./api/server');
 
-const server = require('./api/server')
+require('dotenv').config();
 
 const PORT = process.env.PORT || 9000
 
 server.listen(PORT, () =>  {
     console.log(`listening on port ${PORT}`)
+});
+
+server.use('*', (req, res) => {
+    res.send('<h1>Hello Heroku</h1>')
 })
